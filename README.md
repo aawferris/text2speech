ffbbfea601527ee63174b12db3d06c1335e4b349
Andy Ferris <aawferris@aawferris.home>
2021-01-07 15:18:08 -0500

    followed tutorial and added html, css and JS. Updated js to es6.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # text2speech
+
+## Overview
+
+This is a tutorial that I followed to make a speech to text converter that can be used to increase the accessibility on any website. I followed the tutorial from: https://blog.eduonix.com/artificial-intelligence/speech-text-conversion-using-javascript/.
+
+## Changelog
+
+- I have updated some of the JS to ES6
+- I am changing the style of the page.
+- This would be a great project to convert into a React app component so that it could be used on any SPAs that I make.
---
+++ app.js
@@ -0,0 +1,44 @@
+const SpeechRecognition = window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+const Textbox = $("#textbox");
+const instructions = $("instructions");
+
+const Content = "";
+
+recognition.continuous = true;
+
+recognition.onresult = function (event) {
+  let current = event.resultIndex;
+
+  let transcript = event.results[current][0].transcript;
+
+  Content += transcript;
+  Textbox.val(Content);
+};
+
+recognition.onstart = function () {
+  instructions.text("Voice recognition is ON.");
+};
+
+recognition.onspeechend = function () {
+  instructions.text("No activity.");
+};
+
+recognition.onerror = function (event) {
+  if (event.error == "no-speech") {
+    instructions.text("Try again.");
+  }
+};
+
+$("#start-btn").on("click", function (e) {
+  if (Content.length) {
+    Content += " ";
+  }
+  recognition.start();
+});
+
+Textbox.on("input", function () {
+  Content = $(this).val();
+});
